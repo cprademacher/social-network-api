de9b9ef266c726a60c110a469fb27a236a3ff71f
Cody Rademacher <66492356+cprademacher@users.noreply.github.com>
2023-10-13 10:09:24 -0500

    added beginning server.js code

---
+++ server.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const db = require("./config/connection");
+// Require model
+const { User } = require("./models");
+
+const PORT = process.env.PORT || 3001;
+const app = express();
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// Set routes here
+
+// Set to start server
+db.once("open", () => {
+  app.listen(PORT, () => {
+    console.log(`API server running on port ${PORT}`);
+  });
+});
