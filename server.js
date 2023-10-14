d5831eb09c07a3ac983d255270890d5d6c80fc60
Cody Rademacher <66492356+cprademacher@users.noreply.github.com>
2023-10-14 07:09:36 -0500

    Added get route for users

--- server.js
+++ server.js
@@ -11,6 +11,28 @@ app.use(express.json());
 
 // Set routes here
 
+// GET all users
+app.get("/all-users", async (req, res) => {
+  try {
+    const userData = await User.find({});
+    res.status(200).json(userData);
+  } catch (err) {
+    res.status(500).json({ message: "Internal server error" });
+  }
+});
+
+// GET a single user by its _id and populated thought and friend data
+app.get("/user-by-id/:id", (req, res) => {});
+
+// POST a new user
+app.post("/new-user/:username/:email", (req, res) => {});
+
+// PUT route to update a user by its _id
+app.put("/update-user-by-id/:id", (req, res) => {});
+
+// DELETE route to remove a user by its _id
+app.delete("/delete-user-by-id/:id", (req, res) => {});
+
 // Set to start server
 db.once("open", () => {
   app.listen(PORT, () => {
