8bdfade52aecdf5f05d9208eaad435c589c2a347
Cody Rademacher <66492356+cprademacher@users.noreply.github.com>
2023-10-14 08:25:11 -0500

    Added schema data

--- models/User.js
+++ models/User.js
@@ -1,14 +1,6 @@
 // Require schema and model from mongoose
 const mongoose = require("mongoose");
 
-// Define a new schema named `thoughtSchema` for the subdocument
-const thoughtSchema = new mongoose.Schema({
-  thoughtText: { type: String, required: true, minLength: 1, maxLength: 280 },
-  createdAt: { type: Date, default: Date.now },
-  username: { type: String, required: true },
-  reactions: [reactionSchema],
-});
-
 // Define a new schema named 'reactionSchema' for the subdocument
 const reactionSchema = new mongoose.Schema({
   reactionId: {
@@ -30,6 +22,14 @@ const reactionSchema = new mongoose.Schema({
   },
 });
 
+// Define a new schema named `thoughtSchema` for the subdocument
+const thoughtSchema = new mongoose.Schema({
+  thoughtText: { type: String, required: true, minLength: 1, maxLength: 280 },
+  createdAt: { type: Date, default: Date.now },
+  username: { type: String, required: true },
+  reactions: [reactionSchema],
+});
+
 // Define a new schema named `friendSchema` for the subdocument
 const friendSchema = new mongoose.Schema({});
 
@@ -45,3 +45,52 @@ const userSchema = new mongoose.Schema({
   thoughts: [thoughtSchema],
   friends: [friendSchema],
 });
+
+const reactionData = [
+  {
+    reactionBody: "This is awesome!",
+    username: "Cody",
+  },
+  {
+    reactionBody: "This is not as awesome!",
+    username: "Cody",
+  },
+];
+
+const thoughtData = [
+  {
+    thoughtText: "This is the first thought.",
+    username: "Cody",
+    reactions: [reactionSchema],
+  },
+  {
+    thoughtText: "This is the second thought",
+    username: "Cody",
+    reactions: [reactionSchema],
+  },
+];
+
+const User = mongoose.model("User", userSchema);
+
+User.create({
+  username: "Cody",
+  email: "cprademacher36@gmail.com",
+})
+  .then((data) => console.log(data))
+  .catch((err) => console.log(err));
+
+User.create({
+  username: "Megan",
+  email: "megrad77@gmail.com",
+})
+  .then((data) => console.log(data))
+  .catch((err) => console.log(err));
+
+User.create({
+  username: "Lane",
+  email: "laneparr@gmail.com",
+})
+  .then((data) => console.log(data))
+  .catch((err) => console.log(err));
+
+module.exports = User;
