403e5efe9dfe56442ec058b4b838752218696fa5
Cody Rademacher <66492356+cprademacher@users.noreply.github.com>
2023-10-16 08:30:01 -0500

    Added find user by id and delete by id, started put request

--- server.js
+++ server.js
@@ -17,22 +17,46 @@ app.get("/all-users", async (req, res) => {
     const userData = await User.find({});
     res.status(200).json(userData);
   } catch (err) {
-    res.status(500).json({ message: "Internal server error this one" });
-    console.log(err);
+    res.status(500).json({ message: "Internal server error." });
   }
 });
 
 // GET a single user by its _id and populated thought and friend data
-app.get("/user-by-id/:id", (req, res) => {});
+app.get("/user-by-id/:id", async (req, res) => {
+  const userId = req.params.id;
+
+  try {
+    const response = await User.findOne({ _id: userId });
+    res.status(200).json(response);
+  } catch (err) {
+    res.status(500).json({ message: "Internal server error." });
+  }
+});
 
 // POST a new user
 app.post("/new-user/:username/:email", (req, res) => {});
 
 // PUT route to update a user by its _id
-app.put("/update-user-by-id/:id", (req, res) => {});
+app.put("/update-user-by-id/:id", async (req, res) => {
+  const userId = req.params.id;
+
+  try {
+  } catch (err) {
+    res.status(500).json({ message: "Internal server error." });
+  }
+});
 
 // DELETE route to remove a user by its _id
-app.delete("/delete-user-by-id/:id", (req, res) => {});
+app.delete("/delete-user-by-id/:id", async (req, res) => {
+  const userId = req.params.id;
+
+  try {
+    const response = await User.findOneAndDelete({ _id: userId });
+    res.status(200).json(response);
+  } catch (err) {
+    res.status(500).json({ message: "Internal server error." });
+  }
+});
 
 // Set to start server
 db.once("open", () => {
