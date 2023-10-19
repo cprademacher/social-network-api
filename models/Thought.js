fa882d5c0498563cb58f252e8f0f376ea57f2fb0
Cody Rademacher <66492356+cprademacher@users.noreply.github.com>
2023-10-19 10:33:41 -0500

    Added more models, routes, and controller files

---
+++ models/Thought.js
@@ -0,0 +1,35 @@
+// Require schema and model from mongoose
+const { Schema, model } = require("mongoose");
+
+// Define a new schema named `thoughtSchema` for the subdocument
+const thoughtSchema = new mongoose.Schema(
+  {
+    thoughtText: { type: String, required: true, minLength: 1, maxLength: 280 },
+    createdAt: {
+      type: Date,
+      default: Date.now,
+      get: (timestamp) => dateFormat(timestamp),
+    },
+    user: { type: String, ref: "User" },
+    reactions: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "reaction",
+      },
+    ],
+  },
+  {
+    toJSON: {
+      virtuals: true,
+    },
+    id: false,
+  }
+);
+
+thoughtSchema.virtuals("thoughtCount").get(function () {
+  return this.reactions.length;
+});
+
+const Thought = model("thought", thoughtSchema);
+
+module.exports = Thought;
--- models/User.js
+++ models/User.js
@@ -1,62 +1,46 @@
 // Require schema and model from mongoose
-const mongoose = require("mongoose");
-
-// Define a new schema named 'reactionSchema' for the subdocument
-const reactionSchema = new mongoose.Schema({
-  reactionId: {
-    type: mongoose.Schema.Types.ObjectId,
-    default: () => new mongoose.Types.ObjectId(),
-  },
-  reactionBody: {
-    type: String,
-    required: true,
-    maxlength: 280,
-  },
-  username: {
-    type: String,
-    required: true,
-  },
-  createdAt: {
-    type: Date,
-    default: Date.now,
-    get: (timestamp) => dateFormat(timestamp),
-  },
-});
-
-// Define a new schema named `thoughtSchema` for the subdocument
-const thoughtSchema = new mongoose.Schema({
-  thoughtText: { type: String, required: true, minLength: 1, maxLength: 280 },
-  createdAt: {
-    type: Date,
-    default: Date.now,
-    get: (timestamp) => dateFormat(timestamp),
-  },
-  user: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
-  reactions: [reactionSchema],
-});
+const { Schema, model } = require("mongoose");
 
 // Construct a new instance of the schema class
-const userSchema = new mongoose.Schema({
-  username: { type: String, unique: true, required: true, trim: true },
-  email: {
-    type: String,
-    unique: true,
-    required: true,
-    match: /^[\w-]+(\.[\w-]+)*@[\w-]+(\.[\w-]+)+$/,
-  },
-  thoughts: [thoughtSchema],
-  friends: [
-    {
-      type: mongoose.Schema.Types.ObjectId,
-      ref: "User",
+const userSchema = new mongoose.Schema(
+  {
+    username: { type: String, unique: true, required: true, trim: true },
+    email: {
+      type: String,
+      unique: true,
+      required: true,
+      match: /^[\w-]+(\.[\w-]+)*@[\w-]+(\.[\w-]+)+$/,
     },
-  ],
-});
+    thoughts: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "thought",
+      },
+    ],
+    friends: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "user",
+      },
+    ],
+  },
+  {
+    toJSON: {
+      virtuals: true,
+    },
+    id: false,
+  }
+);
+
+userSchema
+  .virtual("friendCount")
+  // Getter
+  .get(function () {
+    return this.friends.length;
+  });
 
 // Create models from schema
-const User = mongoose.model("User", userSchema);
-const Thought = mongoose.model("Thought", thoughtSchema);
-const Reaction = mongoose.model("Reaction", reactionSchema);
+const User = model("user", userSchema);
 
 const thoughtData = [
   {
@@ -104,11 +88,11 @@ const thoughtData = [
 ];
 
 const newThought = new Thought({
-    thoughtText: "This is a thought",
-    user: "652dbb93c00a8fe0668c3da2",
-  });
-  
-  newThought.save();
+  thoughtText: "This is a thought",
+  user: "652dbb93c00a8fe0668c3da2",
+});
+
+newThought.save();
 
 // Create users, thoughts, and reactions
 User.create({
